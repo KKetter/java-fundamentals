f649e830ed091d6baa7aa507ef6d6a406a37e0a3
Kent Ketter <kent.ketter1@gmail.com>
2019-05-28 13:01:20 -0700

    working flip

--- basics/Main.java
+++ basics/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //declare values for
-        int itemCount = 10;
-        System.out.println("I own " + itemCount + " " + pluralize("dog", itemCount) + ".");
+        //int itemCount = 10;
+        //System.out.println("I own " + itemCount + " " + pluralize("dog", itemCount) + ".");
+        //flipNHeads(3);
+        clock();
     }
 
     public static String pluralize(String item, int itemCount) {
@@ -11,6 +12,29 @@ public class Main {
         } else {
             return (item + "s");
         }
+    }
+
+    public static void flipNHeads(int flipStreakGoal) {
+        int runCounter = 0;
+        int totalCounter = 0;
+        //while loop with until counter = flipStreakGoal continue?
+        while(flipStreakGoal != runCounter) {
+            double flipValue = Math.random();
+            if(flipValue >= .5){
+                runCounter++; 
+                System.out.println("heads");
+            } else {
+                runCounter = 0;
+                System.out.println("tails");
+            }
+            totalCounter++;
+        }
+        //inside loop conditions randomNumGen set to variable - if >=.5 = heads/else = tails
+        System.out.println("It took " + totalCounter + " flips to flip " + flipStreakGoal + " heads in a row.");
+    }
+
+    public static void clock() {
+
     }
 }
 
